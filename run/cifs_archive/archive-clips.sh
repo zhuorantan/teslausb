8be6a913d2d9c525cb2f3a336d59b1536282f7a6
Marco Nelissen <marcone@google.com>
2019-04-08 18:29:24 -0700

    CIFS: Don't archive short files
    Turning off the car, or the unpublishing of the card when in wi-fi range, may
    result in truncated, unplayable files. This change makes it so files smaller
    than 100000 bytes are deleted instead of archived.
    Also remove empty directories under SavedClips after archiving.

--- run/cifs_archive/archive-clips.sh
+++ run/cifs_archive/archive-clips.sh
@@ -3,6 +3,8 @@
 log "Moving clips to archive..."
 
 NUM_FILES_MOVED=0
+NUM_FILES_FAILED=0
+NUM_FILES_DELETED=0
 
 function moveclips() {
   ROOT="$1"
@@ -22,15 +24,24 @@ function moveclips() {
       mkdir -p "$ARCHIVE_MOUNT/$file_name"
     elif [ -f "$ROOT/$file_name" ]
     then
-      log "Moving '$file_name'"
-      outdir=$(dirname "$file_name")
-      if mv -f "$ROOT/$file_name" "$ARCHIVE_MOUNT/$outdir"
+      size=$(stat -c%s "$ROOT/$file_name")
+      if [ $size -lt 100000 ]
       then
-        log "Moved '$file_name'"
+        log "'$file_name' is only $size bytes"
+        rm "$ROOT/$file_name"
+        NUM_FILES_DELETED=$((NUM_FILES_DELETED + 1))
       else
-        log "Failed to move '$file_name'"
+        log "Moving '$file_name'"
+        outdir=$(dirname "$file_name")
+        if mv -f "$ROOT/$file_name" "$ARCHIVE_MOUNT/$outdir"
+        then
+          log "Moved '$file_name'"
+          NUM_FILES_MOVED=$((NUM_FILES_MOVED + 1))
+        else
+          log "Failed to move '$file_name'"
+          NUM_FILES_FAILED=$((NUM_FILES_FAILED + 1))
+        fi
       fi
-      NUM_FILES_MOVED=$((NUM_FILES_MOVED + 1))
     else
       log "$file_name not found"
     fi
@@ -43,7 +54,10 @@ moveclips "$CAM_MOUNT/TeslaCam" 'saved*'
 # new file name pattern, firmware 2019.*
 moveclips "$CAM_MOUNT/TeslaCam/SavedClips" '*'
 
-log "Moved $NUM_FILES_MOVED file(s)."
+# delete empty directories under SavedClips
+rmdir --ignore-fail-on-non-empty "$CAM_MOUNT/TeslaCam/SavedClips"/* || true
+
+log "Moved $NUM_FILES_MOVED file(s), failed to copy $NUM_FILES_FAILED, deleted $NUM_FILES_DELETED."
 
 if [ $NUM_FILES_MOVED -gt 0 ]
 then
