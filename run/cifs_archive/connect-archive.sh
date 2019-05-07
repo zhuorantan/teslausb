4dad1de044b323797dc894307c3dafff920d6c4d
Marco Nelissen <marcone@google.com>
2019-05-06 20:03:43 -0700

    Fix race condition that could cause archiveloop to exit
    If the wifi connection went away between the time that archiveloop
    detected that the archive was reachable, and the time that it
    actually connected to the archive, the resulting failure to mount
    the archive would cause archiveloop to terminate.
    
    archiveloop now checks whether mounting succeeded, and skips the
    archive step if it didn't.

--- run/cifs_archive/connect-archive.sh
+++ run/cifs_archive/connect-archive.sh
@@ -2,13 +2,23 @@
 
 function ensure_archive_is_mounted () {
   log "Ensuring cam archive is mounted..."
-  ensure_mountpoint_is_mounted_with_retry "$ARCHIVE_MOUNT"
-  log "Ensured cam archive is mounted."
+  if ensure_mountpoint_is_mounted_with_retry "$ARCHIVE_MOUNT"
+  then
+    log "Ensured cam archive is mounted."
+  else
+    log "Failed to mount cam archive."
+    return 1
+  fi
   if [ -e "$MUSIC_ARCHIVE_MOUNT" ]
   then
     log "Ensuring music archive is mounted..."
-    ensure_mountpoint_is_mounted_with_retry "$MUSIC_ARCHIVE_MOUNT"
-    log "Ensured music archive is mounted."
+    if ensure_mountpoint_is_mounted_with_retry "$MUSIC_ARCHIVE_MOUNT"
+    then
+      log "Ensured music archive is mounted."
+    else
+      log "Failed to mount music archive."
+      return 1
+    fi
   fi
 }
 
