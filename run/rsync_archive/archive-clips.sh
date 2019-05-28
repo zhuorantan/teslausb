11355ddf83458f295a6f75ae1e4d80882e5329a6
David Shay <davideshay@gmail.com>
2019-05-28 19:50:26 -0400

    Additional fixes to rsync version of archive-clips
    Was not performing correct integer comparison, so code was never executed

--- run/rsync_archive/archive-clips.sh
+++ run/rsync_archive/archive-clips.sh
@@ -8,7 +8,7 @@ num_files_moved=$(rsync -auvh --remove-source-files --no-perms --stats --log-fil
 
 /root/bin/send-push-message "$num_files_moved"
 
-if [ $num_files_moved > 0 ]
+if (( $num_files_moved > 0 ))
 then
   find /mnt/cam/Teslacam/SavedClips/ -depth -type d -empty -exec rmdir "{}" \;
   log "Successfully synced files through rsync."
