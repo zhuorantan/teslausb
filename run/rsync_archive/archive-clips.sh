8a809d01ebbee5ba8f402b76105f8bd3c405ebbf
Marco Nelissen <marcone@google.com>
2019-06-09 15:25:13 -0700

    Remove hardcoded path in rsync archive-clips.sh

--- run/rsync_archive/archive-clips.sh
+++ run/rsync_archive/archive-clips.sh
@@ -4,13 +4,13 @@ log "Archiving through rsync..."
 
 source /root/.teslaCamRsyncConfig
 
-num_files_moved=$(rsync -auvh --remove-source-files --no-perms --stats --log-file=/tmp/archive-rsync-cmd.log /mnt/cam/TeslaCam/saved* /mnt/cam/TeslaCam/SavedClips/* $user@$server:$path | awk '/files transferred/{print $NF}')
+num_files_moved=$(rsync -auvh --remove-source-files --no-perms --stats --log-file=/tmp/archive-rsync-cmd.log $CAM_MOUNT/TeslaCam/saved* $CAM_MOUNT/TeslaCam/SavedClips/* $user@$server:$path | awk '/files transferred/{print $NF}')
 
 /root/bin/send-push-message "$num_files_moved"
 
 if (( $num_files_moved > 0 ))
 then
-  find /mnt/cam/Teslacam/SavedClips/ -depth -type d -empty -exec rmdir "{}" \;
+  find $CAM_MOUNT/Teslacam/SavedClips/ -depth -type d -empty -exec rmdir "{}" \;
   log "Successfully synced files through rsync."
 else
   log "No files to archive through rsync."
