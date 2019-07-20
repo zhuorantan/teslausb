ddc1e11e24fbed0d80d481ee1ab1e2acd0ea9588
Marco Nelissen <marcone@google.com>
2019-07-19 20:55:54 -0700

    Umount CIFS mount after archiving
    Without this, the archive mounts frequently get into a state where the
    archive is reachable via the network, appears to be mounted, but the
    mount is inoperable and any attempt to access it results in a "host is
    down" message

--- run/cifs_archive/disconnect-archive.sh
+++ run/cifs_archive/disconnect-archive.sh
@@ -1,2 +1,21 @@
 #!/bin/bash -eu
-# Nothing to do. It's okay to leave the archive mounted.
+
+# unmount the archive. Without this, the archive mounts can get into a
+# state where the archive is reachable via the network, appears to be
+# mounted, but the mount is inoperable and any attempt to access it
+# results in a "host is down" message.
+
+log "unmounting $ARCHIVE_MOUNT"
+if ! umount -f -l "$ARCHIVE_MOUNT"
+then
+  log "unmount failed"
+fi
+
+if [ -e "$MUSIC_ARCHIVE_MOUNT" ]
+then
+  log "unmounting $MUSIC_ARCHIVE_MOUNT"
+  if ! umount -f -l "$MUSIC_ARCHIVE_MOUNT"
+  then
+    log "unmount failed"
+  fi
+fi
