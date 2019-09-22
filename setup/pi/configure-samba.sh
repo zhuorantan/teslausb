a182f6070695697f4ed38d17bb09c45dce104bba
Marco Nelissen <marco.nelissen@gmail.com>
2019-09-22 08:37:32 -0700

    Rework samba install to work on Buster
    Ensure samba installation also works when samba folders
    do not exist, and prevent interactive dialogs from being
    shown during install.

--- setup/pi/configure-samba.sh
+++ setup/pi/configure-samba.sh
@@ -16,7 +16,56 @@ else
   GUEST_OK="no"
 fi
 
-# update smb.conf in case we're updating a previous install
+if ! hash smbd &> /dev/null
+then
+  log_progress "Installing samba and dependencies..."
+  # before installing, move some of samba's folders off of the
+  # soon-to-be-readonly root partition
+
+  mkdir -p /var/cache/samba
+  mkdir -p /var/run/samba
+
+  if ! grep -q samba /etc/fstab
+  then
+    echo "tmpfs /var/run/samba tmpfs nodev,nosuid 0 0" >> /etc/fstab
+    echo "tmpfs /var/cache/samba tmpfs nodev,nosuid 0 0" >> /etc/fstab
+  fi
+
+  mount /var/cache/samba
+  mount /var/run/samba
+
+  if [ ! -L /var/lib/samba ]
+  then
+    if ! findmnt --mountpoint /mutable
+    then
+        mount /mutable
+    fi
+
+    mkdir -p /mutable/varlib
+    if [ -d /var/lib/samba ]
+    then
+      mv /var/lib/samba /mutable/varlib
+    else
+      mkdir /mutable/varlib/samba
+    fi
+    ln -s /mutable/varlib/samba /var/lib/samba
+  fi
+
+  # directory where the snapshots will be mounted and exported by samba
+  if [ ! -e /mnt/smbexport ]
+  then
+    mkdir /mnt/smbexport
+    echo "tmpfs /mnt/smbexport tmpfs nodev,nosuid 0 0" >> /etc/fstab
+  fi
+
+  DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install samba
+  service smbd start
+  echo -e "raspberry\nraspberry\n" | smbpasswd -s -a pi
+  service smbd stop
+  log_progress "Done."
+fi
+
+# always update smb.conf in case we're updating a previous install
 cat <<- EOF > /etc/samba/smb.conf
 	[global]
 	   deadtime = 2
@@ -48,47 +97,3 @@ cat <<- EOF > /etc/samba/smb.conf
 	   delete veto files = yes
 	   root preexec = /root/bin/make_snapshot.sh
 	EOF
-
-if ! hash smbd &> /dev/null
-then
-  log_progress "Installing samba and dependencies..."
-  # before installing, move some of samba's folders off of the
-  # soon-to-be-readonly root partition
-
-  mkdir -p /var/cache/samba
-  mkdir -p /var/run/samba
-
-  if ! grep -q samba /etc/fstab
-  then
-    echo "tmpfs /var/run/samba tmpfs nodev,nosuid 0 0" >> /etc/fstab
-    echo "tmpfs /var/cache/samba tmpfs nodev,nosuid 0 0" >> /etc/fstab
-  fi
-
-  mount /var/cache/samba
-  mount /var/run/samba
-
-  if [ ! -L /var/lib/samba ]
-  then
-    if ! findmnt --mountpoint /mutable
-    then
-        mount /mutable
-    fi
-
-    mkdir -p /mutable/varlib
-    mv /var/lib/samba /mutable/varlib
-    ln -s /mutable/varlib/samba /var/lib/samba
-  fi
-
-  # directory where the snapshots will be mounted and exported by samba
-  if [ ! -e /mnt/smbexport ]
-  then
-    mkdir /mnt/smbexport
-    echo "tmpfs /mnt/smbexport tmpfs nodev,nosuid 0 0" >> /etc/fstab
-  fi
-
-  apt-get -y --force-yes install samba
-  service smbd start
-  echo -e "raspberry\nraspberry\n" | smbpasswd -s -a pi
-  service smbd stop
-  log_progress "Done."
-fi
