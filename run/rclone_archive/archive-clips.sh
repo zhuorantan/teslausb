564e9c4d435114851482866be3b1d8d2fb8de3b1
carleeno <54586926+carleeno@users.noreply.github.com>
2019-09-29 22:55:55 -0400

    sub dirs and filecount fix
    Changed to backup to Saved vs Sentry sub dirs, same as PR 280
    Fixed filecount, copied same code from archiveloop

--- run/rclone_archive/archive-clips.sh
+++ run/rclone_archive/archive-clips.sh
@@ -4,16 +4,15 @@ log "Moving clips to rclone archive..."
 
 source /root/.teslaCamRcloneConfig
 
-FILE_COUNT=$(cd "$CAM_MOUNT/TeslaCam" && find . -path './SavedClips' -o -path './SentryClips' -type f | wc -l)
+FILE_COUNT=$(cd "$CAM_MOUNT"/TeslaCam && find . -maxdepth 3 -path './SavedClips/*' -o -path './SentryClips/*' -type f | wc -l)
 
-rclone --config /root/.config/rclone/rclone.conf move "$CAM_MOUNT"/TeslaCam/SavedClips "$drive:$path" --create-empty-src-dirs --delete-empty-src-dirs >> "$LOG_FILE" 2>&1 || echo ""
-rclone --config /root/.config/rclone/rclone.conf move "$CAM_MOUNT"/TeslaCam/SentryClips "$drive:$path" --create-empty-src-dirs --delete-empty-src-dirs >> "$LOG_FILE" 2>&1 || echo ""
+rclone --config /root/.config/rclone/rclone.conf move "$CAM_MOUNT"/TeslaCam/SavedClips "$drive:$path"/SavedClips/ --create-empty-src-dirs --delete-empty-src-dirs >> "$LOG_FILE" 2>&1 || echo ""
+rclone --config /root/.config/rclone/rclone.conf move "$CAM_MOUNT"/TeslaCam/SentryClips "$drive:$path"/SentryClips/ --create-empty-src-dirs --delete-empty-src-dirs >> "$LOG_FILE" 2>&1 || echo ""
 
-FILES_REMAINING=$(cd "$CAM_MOUNT/TeslaCam" && find . -path './SavedClips' -o -path './SentryClips' -type f | wc -l)
+FILES_REMAINING=$(cd "$CAM_MOUNT"/TeslaCam && find . -maxdepth 3 -path './SavedClips/*' -o -path './SentryClips/*' -type f | wc -l)
 NUM_FILES_MOVED=$((FILE_COUNT-FILES_REMAINING))
 
 log "Moved $NUM_FILES_MOVED file(s)."
 /root/bin/send-push-message "TeslaUSB:" "Moved $NUM_FILES_MOVED dashcam file(s)."
-
 
 log "Finished moving clips to rclone archive"
