06bd36332fc4102f022c10f609272a754bddd9a4
rhcp011235 <rhcp011235@me.com>
2019-10-29 14:59:14 -0400

    Typo Fix : Credit to keno

--- run/cifs_archive/archive-clips.sh
+++ run/cifs_archive/archive-clips.sh
@@ -6,7 +6,7 @@ num_files_moved=$(rsync -auvh --timeout=60 --remove-source-files --no-perms --st
 
 if (( $num_files_moved > 0 ))
 then
-  find $CAM_MOUNT/Teslacam/SavedClips/ $CAM_MOUNT/Teslacam/SentryClips/ -depth -type d -empty -exec rmdir "{}" \;
+  find $CAM_MOUNT/TeslaCam/SavedClips/ $CAM_MOUNT/TeslaCam/SentryClips/ -depth -type d -empty -exec rmdir "{}" \;
   log "Successfully synced $num_files_moved files through rsync."
   /root/bin/send-push-message "TeslaUSB:" "Moved $num_files_moved dashcam files"
 else
