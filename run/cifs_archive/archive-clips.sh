5fae7864c669d1405db0e2c9c5a5911c085df550
Marco Nelissen <marco.nelissen@gmail.com>
2019-10-30 18:21:08 -0700

    Revert "use rsync over CIFS (#319)"
    This reverts commit 58865ffad2f9a37a17b99ca5e9f5e49348151eef.
    
    Reason for revert: rsync's --timeout option does not work when
    copying files from one directory to another, and as a result
    it can take a really long time to time out when moving out of
    wifi range while archiving.

--- run/cifs_archive/archive-clips.sh
+++ run/cifs_archive/archive-clips.sh
@@ -1,16 +1,96 @@
 #!/bin/bash -eu
 
-log "syncing clips to archive..."
+log "Moving clips to archive..."
 
-num_files_moved=$(rsync -auvh --timeout=60 --remove-source-files --no-perms --stats --log-file=/tmp/archive-rsync-cmd.log $CAM_MOUNT/TeslaCam/SentryClips $CAM_MOUNT/TeslaCam/SavedClips $ARCHIVE_MOUNT/ | awk '/files transferred/{print $NF}')
+NUM_FILES_MOVED=0
+NUM_FILES_FAILED=0
+NUM_FILES_DELETED=0
 
-if (( $num_files_moved > 0 ))
+function connectionmonitor {
+  while true
+  do
+    for i in $(seq 1 10)
+    do
+      if timeout 3 /root/bin/archive-is-reachable.sh $ARCHIVE_HOST_NAME
+      then
+        # sleep and then continue outer loop
+        sleep 5
+        continue 2
+      fi
+    done
+    log "connection dead, killing archive-clips"
+    # The archive loop might be stuck on an unresponsive server, so kill it hard.
+    # (should be no worse than losing power in the middle of an operation)
+    kill -9 $1
+    return
+  done
+}
+
+function moveclips() {
+  ROOT="$1"
+  PATTERN="$2"
+  SUB=$(basename $ROOT)
+
+  if [ ! -d "$ROOT" ]
+  then
+    log "$ROOT does not exist, skipping"
+    return
+  fi
+
+  while read file_name
+  do
+    if [ -d "$ROOT/$file_name" ]
+    then
+      log "Creating output directory '$SUB/$file_name'"
+      if ! mkdir -p "$ARCHIVE_MOUNT/$SUB/$file_name"
+      then
+        log "Failed to create '$SUB/$file_name', check that archive server is writable and has free space"
+        return
+      fi
+    elif [ -f "$ROOT/$file_name" ]
+    then
+      size=$(stat -c%s "$ROOT/$file_name")
+      if [ $size -lt 100000 ]
+      then
+        log "'$SUB/$file_name' is only $size bytes"
+        rm "$ROOT/$file_name"
+        NUM_FILES_DELETED=$((NUM_FILES_DELETED + 1))
+      else
+        log "Moving '$SUB/$file_name'"
+        outdir=$(dirname "$file_name")
+        if mv -f "$ROOT/$file_name" "$ARCHIVE_MOUNT/$SUB/$outdir"
+        then
+          log "Moved '$SUB/$file_name'"
+          NUM_FILES_MOVED=$((NUM_FILES_MOVED + 1))
+        else
+          log "Failed to move '$SUB/$file_name'"
+          NUM_FILES_FAILED=$((NUM_FILES_FAILED + 1))
+        fi
+      fi
+    else
+      log "$SUB/$file_name not found"
+    fi
+  done <<< $(cd "$ROOT"; find $PATTERN)
+}
+
+connectionmonitor $$ &
+
+# new file name pattern, firmware 2019.*
+moveclips "$CAM_MOUNT/TeslaCam/SavedClips" '*'
+
+# v10 firmware adds a SentryClips folder
+moveclips "$CAM_MOUNT/TeslaCam/SentryClips" '*'
+
+kill %1
+
+# delete empty directories under SavedClips and SentryClips
+rmdir --ignore-fail-on-non-empty "$CAM_MOUNT/TeslaCam/SavedClips"/* "$CAM_MOUNT/TeslaCam/SentryClips"/* || true
+
+log "Moved $NUM_FILES_MOVED file(s), failed to copy $NUM_FILES_FAILED, deleted $NUM_FILES_DELETED."
+
+if [ $NUM_FILES_MOVED -gt 0 ]
 then
-  find $CAM_MOUNT/TeslaCam/SavedClips/ $CAM_MOUNT/TeslaCam/SentryClips/ -depth -type d -empty -exec rmdir "{}" \;
-  log "Successfully synced $num_files_moved files through rsync."
-  /root/bin/send-push-message "TeslaUSB:" "Moved $num_files_moved dashcam files"
-else
-  log "No files archived."
+  /root/bin/send-push-message "TeslaUSB:" "Moved $NUM_FILES_MOVED dashcam file(s), failed to copy $NUM_FILES_FAILED, deleted $NUM_FILES_DELETED."
 fi
 
-
+log "Finished moving clips to archive."
