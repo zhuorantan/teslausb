09b33cf895cec31810daf0feb3276cd108d9d80c
Robert J. Moore <Robert.J.Moore@allanbank.com>
2019-11-03 19:44:40 -0500

    Have the CIFS copy-music.sh delete files that no longer exist on the archive server.
    Fixes #293

--- run/cifs_archive/copy-music.sh
+++ run/cifs_archive/copy-music.sh
@@ -5,6 +5,9 @@ log "Copying music from archive..."
 NUM_FILES_COPIED=0
 NUM_FILES_SKIPPED=0
 NUM_FILES_ERROR=0
+NUM_FILES_DELETED=0
+NUM_FILES_DELETE_ERROR=0
+
 SRC="/mnt/musicarchive"
 DST="/mnt/music"
 
@@ -36,6 +39,23 @@ fi
 
 connectionmonitor $$ &
 
+# Delete files from the local partition(DST) files that do not exist in the 
+# music archive(SRC). This frees space for the new files that may be added.
+while read file_name
+do
+  if [ ! -e "$SRC/$file_name" ]
+  then
+    if rm "$DST/$file_name"
+    then
+      NUM_FILES_DELETED=$((NUM_FILES_DELETED + 1))
+    else
+      log "Couldn't delete $DST/$file_name"
+      NUM_FILES_DELETE_ERROR=$((NUM_FILES_DELETE_ERROR + 1))
+    fi
+  fi
+done <<< "$(cd "$DST"; find * -type f)"
+
+# Copy from the music archive(SRC) to the local parition(DST)
 while read file_name
 do
   if [ ! -e "$DST/$file_name" ]
@@ -65,11 +85,12 @@ do
   fi
 done <<< "$(cd "$SRC"; find * -type f)"
 
+# Stop the connection monitor.
 kill %1
 
-log "Copied $NUM_FILES_COPIED music file(s), skipped $NUM_FILES_SKIPPED previously-copied files, encountered $NUM_FILES_ERROR errors."
+log "Copied $NUM_FILES_COPIED music file(s), deleted $NUM_FILES_DELETED, skipped $NUM_FILES_SKIPPED previously-copied files, encountered $NUM_FILES_ERROR copy errors and $NUM_FILES_DELETE_ERROR delete errors."
 
 if [ $NUM_FILES_COPIED -gt 0 ]
 then
-  /root/bin/send-push-message "TeslaUSB:" "Copied $NUM_FILES_COPIED music file(s), skipped $NUM_FILES_SKIPPED previously-copied files, encountered $NUM_FILES_ERROR errors."
+  /root/bin/send-push-message "TeslaUSB:" "Copied $NUM_FILES_COPIED music file(s), deleted $NUM_FILES_DELETED, skipped $NUM_FILES_SKIPPED previously-copied files, encountered $NUM_FILES_ERROR copy errors and $NUM_FILES_DELETE_ERROR delete errors."
 fi
