eb025769974f02e7ddb87faffd19b9a4a73e2c7c
Marco Nelissen <marco.nelissen@gmail.com>
2019-11-11 16:00:16 -0800

    Fix force_sync script

--- run/force_sync.sh
+++ run/force_sync.sh
@@ -2,12 +2,12 @@
 
 printf "Forcing archiveloop to sync by pretending to take archive host offline and back online.\n"
 printf "Setting archive unreachable..\n"
-if ! touch /tmp/archive-is-unreachable
+if ! touch /tmp/archive_is_unreachable
 then
   printf "Something went wrong!  Error %d\nAborting.\n" "$?" 1>&2
   exit 1
 fi
-while [[ -f /tmp/archive-is-unreachable ]]
+while [[ -f /tmp/archive_is_unreachable ]]
 do
   printf "Waiting for archiveloop to see canary file..\n"
   sleep 5
