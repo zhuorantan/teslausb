1a0e3b419cf927a5bfbf9782f6ffeb85261a63fd
Marco Nelissen <marco.nelissen@gmail.com>
2019-12-05 17:19:13 -0800

    unmount archive in the background
    Forced+lazy unmount can still block the umount command when network
    connectivity is lost. Run the unmount in the background so it can
    resume the archiveloop right away.

--- run/cifs_archive/disconnect-archive.sh
+++ run/cifs_archive/disconnect-archive.sh
@@ -4,18 +4,23 @@
 # state where the archive is reachable via the network, appears to be
 # mounted, but the mount is inoperable and any attempt to access it
 # results in a "host is down" message.
+# Run this in the background, since unmounting can hang, which would
+# block a return to archiveloop.
 
-log "unmounting $ARCHIVE_MOUNT"
-if ! umount -f -l "$ARCHIVE_MOUNT"
-then
-  log "unmount failed"
-fi
-
-if [ -e "$MUSIC_ARCHIVE_MOUNT" ]
-then
-  log "unmounting $MUSIC_ARCHIVE_MOUNT"
-  if ! umount -f -l "$MUSIC_ARCHIVE_MOUNT"
+{
+  log "unmounting $ARCHIVE_MOUNT"
+  if ! umount -f -l "$ARCHIVE_MOUNT"
   then
     log "unmount failed"
   fi
-fi
+
+  if [ -e "$MUSIC_ARCHIVE_MOUNT" ]
+  then
+    log "unmounting $MUSIC_ARCHIVE_MOUNT"
+    if ! umount -f -l "$MUSIC_ARCHIVE_MOUNT"
+    then
+      log "unmount failed"
+    fi
+  fi
+} &
+
