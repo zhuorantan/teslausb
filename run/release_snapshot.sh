0c5562b1c751f0600887bc1ec76124c8d3eef1b6
Marco Nelissen <marco.nelissen@gmail.com>
2020-04-18 14:10:29 -0700

    Fix shellcheck warnings

--- run/release_snapshot.sh
+++ run/release_snapshot.sh
@@ -4,12 +4,12 @@
 NAME=$(basename "$1")
 LOOPPART=$(mount | grep -w "$NAME" | awk '{print $1}')
 LOOPROOT=${LOOPPART/p1/}
-MNT=$(findmnt -o target -n $LOOPPART)
+MNT=$(findmnt -o target -n "$LOOPPART")
 umount "$MNT"
 losetup -d "$LOOPROOT"
 
 # delete the snapshot folders
-rm -rf /backingfiles/snapshots/$NAME /tmp/snapshots/$NAME
+rm -rf "/backingfiles/snapshots/$NAME" "/tmp/snapshots/$NAME"
 
 # delete all dead links
 find /backingfiles/TeslaCam/ -depth -xtype l -delete || true
