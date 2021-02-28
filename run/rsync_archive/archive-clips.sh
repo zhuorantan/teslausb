e8cbbf095214bcc43957d66f27cce35edae01cab
Marco Nelissen <marco.nelissen@gmail.com>
2021-02-28 12:23:35 -0800

    Don't update directory times when rsyncing

--- run/rsync_archive/archive-clips.sh
+++ run/rsync_archive/archive-clips.sh
@@ -5,6 +5,6 @@ source /root/.teslaCamRsyncConfig
 while [ -n "${1+x}" ]
 do
   # shellcheck disable=SC2154
-  rsync -auvhR --timeout=60 --remove-source-files --no-perms --stats --log-file=/tmp/archive-rsync-cmd.log --files-from="$2" "$1" "$user@$server:$path" &> /tmp/rsynclog
+  rsync -auvhR --timeout=60 --remove-source-files --no-perms --omit-dir-times --stats --log-file=/tmp/archive-rsync-cmd.log --files-from="$2" "$1" "$user@$server:$path" &> /tmp/rsynclog
   shift 2
 done
