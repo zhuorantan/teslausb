64956ee4bda2a3d8d5844597aa45dcba4067b395
Marco Nelissen <marco.nelissen@gmail.com>
2021-03-06 19:46:49 -0800

    Rework archive triggers
    Make the archive trigger files part of the archive operation
    itself, so they work with cifs, rsync and rclone.
    Fixes #617

--- run/cifs_archive/archive-clips.sh
+++ run/cifs_archive/archive-clips.sh
@@ -64,29 +64,4 @@ do
   shift 2
 done
 
-# Create trigger file for SavedClips
-# shellcheck disable=SC2154
-if [ -n "${trigger_file_saved+x}" ]
-then
-  log "Creating SavedClips Trigger File: $ARCHIVE_MOUNT/SavedClips/${trigger_file_saved}"
-  touch "$ARCHIVE_MOUNT/SavedClips/${trigger_file_saved}"
-fi
-
-# Create trigger file for SentryClips
-# shellcheck disable=SC2154
-if [ -n "${trigger_file_sentry+x}" ]
-then
-  log "Creating SentryClips Trigger File: $ARCHIVE_MOUNT/SentryClips/${trigger_file_sentry}"
-  touch "$ARCHIVE_MOUNT/SentryClips/${trigger_file_sentry}"
-fi
-
-# Create trigger file for Archive Root
-# shellcheck disable=SC2154
-if [ -n "${trigger_file_any+x}" ]
-then
-  log "Creating Archive Root Trigger File: $ARCHIVE_MOUNT/${trigger_file_any}"
-  touch "$ARCHIVE_MOUNT/${trigger_file_any}"
-fi
-
 kill %1
-
