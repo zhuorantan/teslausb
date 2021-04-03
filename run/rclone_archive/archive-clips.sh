31dc60063ea79c87ebced0ae376cc731eeabb056
Marco Nelissen <marco.nelissen@gmail.com>
2021-04-02 17:15:34 -0700

    Fix rclone when no flags are specified
    Fixes: #624

--- run/rclone_archive/archive-clips.sh
+++ run/rclone_archive/archive-clips.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -eu
 
+flags=("-L" "--transfers=1")
+if [[ -v RCLONE_FLAGS ]]
+then
+  flags+=("${RCLONE_FLAGS[@]}")
+fi
+
 while [ -n "${1+x}" ]
 do
-  rclone --config /root/.config/rclone/rclone.conf move -L "${RCLONE_FLAGS[@]:-}" --transfers=1 --files-from "$2" "$1" "$RCLONE_DRIVE:$RCLONE_PATH" >> "$LOG_FILE" 2>&1
+  rclone --config /root/.config/rclone/rclone.conf move "${flags[@]}" --files-from "$2" "$1" "$RCLONE_DRIVE:$RCLONE_PATH" >> "$LOG_FILE" 2>&1
   shift 2
 done
